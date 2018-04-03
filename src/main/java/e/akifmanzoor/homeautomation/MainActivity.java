16d5fed8883cefa919c5fe5d69e0e29e8f68e95a
Akif Manzoor <akifmanzoor124@gmail.com>
2018-04-03 12:07:32 -0400

    aa

--- src/main/java/e/akifmanzoor/homeautomation/MainActivity.java
+++ src/main/java/e/akifmanzoor/homeautomation/MainActivity.java
@@ -3,15 +3,15 @@ package e.akifmanzoor.homeautomation;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+
 import android.os.Handler;
-import android.provider.ContactsContract;
-import android.provider.Settings;
+import android.os.Bundle;
+
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,6 +23,8 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import android.provider.Settings;
+import android.view.View;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
